871bb18e709cf6947e6f6ee894aadc2a6595817d
Kjetil Indrehus <kjetilindrehus@gmail.com>
2023-03-23 16:28:04 +0100

    add: sharpness ratio

--- image_score.m
+++ image_score.m
@@ -4,7 +4,9 @@
 function score = image_score(orginal_image_url, secondary_image_url)
     orginal = imread(orginal_image_url);
     secondary = imread(secondary_image_url);
-    score = calculateNoise(secondary)/calculateNoise(orginal);
+    score = calculateNoise(secondary)/calculateNoise(orginal) + ...
+        calculateResolutionDifference(secondary, orginal) + ...
+        sharpnessRatio(orginal,secondary);
 
     
 end
@@ -31,15 +33,48 @@ end
 % Calculates the resolution difference between two images
 
 function diff = calculateResolutionDifference(givenImage, givenImage2)
+    % Get size of the images
+    [m1, n1] = size(givenImage);
+    [m2, n2] = size(givenImage2);
+    
+    % Calculate resolution difference
+    diff = abs(m1*n1 - m2*n2);
+    
+    % Converts the scalar diff to a double precision number
+    diff = double(diff);
 
-% Get size of the images
-[m1, n1] = size(givenImage);
-[m2, n2] = size(givenImage2);
+end
 
-% Calculate resolution difference
-diff = abs(m1*n1 - m2*n2);
 
-% Converts the scalar diff to a double precision number
-diff = double(diff);
+%% Sharpness 
 
+function sharpnessDif = sharpnessRatio(originalImage, secondaryImage)
+    % Sharpness is a subjective measure of the clarity and detail in an image. 
+    % However, there are some objective measures that can be used to estimate the sharpness of an image. 
+    % One such measure is the sharpness ratio, which is the ratio of the high-frequency energy (HFE) to the low-frequency energy (LFE) in the image.
+    
+    
+    % Convert the images to grayscale
+    image1Grey = im2gray(originalImage);
+    image2Grey = im2gray(secondaryImage);
+    
+    % Calculate the size of the images
+    [m,n] = size(image1Grey);
+    
+    % Define the Laplacian filter
+    lap_filter = [0 1 0; 1 -4 1; 0 1 0];
+    
+    % Calculate the high-frequency energy for both images
+    hfe1 = sum(sum(abs(filter2(lap_filter, image1Grey))));
+    hfe2 = sum(sum(abs(filter2(lap_filter, image2Grey))));
+    
+    % Calculate the low-frequency energy for both images
+    lfe1 = sum(sum(image1Grey.^2))/(m*n);
+    lfe2 = sum(sum(image2Grey.^2))/(m*n);
+    
+    % Calculate the sharpness ratio for both images
+    sr1 = hfe1/lfe1;
+    sr2 = hfe2/lfe2;
+    
+    sharpnessDif = sr2/sr1;
 end
