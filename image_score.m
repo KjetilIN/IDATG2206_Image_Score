1602f7f6ab47368a0355105fe13fa1331bf0c4c7
Kjetil Indrehus <kjetilindrehus@gmail.com>
2023-03-23 13:01:45 +0100

    add: testing file

--- image_score.m
+++ image_score.m
@@ -1,13 +1,10 @@
 % The main function that should show the score diffrence provided the image
 % paths
-function image_score(img1_url, img2_url)
-    img1 = imread(img1_url);
-    img2 = imread(img2_url);
+function score = image_score(orginal_image_url, secondary_image_url)
+    orginal = imread(orginal_image_url);
+    secondary = imread(secondary_image_url);
     
-    subplot(1,2,1), imshow(img1), title("Image: " + img1_url);
-    subplot(1,2,2), imshow(img2), title("Image: " + img2_url);
-
-    qualityIndex = calculateNoise(img1);
+    score = calculateNoise(orginal);
 
     
 end
---
+++ testing_main.m
@@ -0,0 +1,5 @@
+%% Testing of diffrent images
+
+% Flower image
+score = image_score("coins.png","coins.png");
+disp("Flower score between blured and original: " + score)
