dd39b5c9d95b7f0192a1f07c949d2c83b9b04c8d
Kjetil Indrehus <kjetilindrehus@gmail.com>
2023-03-30 09:35:34 +0200

    linting

--- compare_modell.m
+++ compare_modell.m
@@ -1,25 +1,41 @@
+%% Compare out index to the SSIM diffrent distortions 
+
+%Original path prefix
 original_path_prefix = "Images/Original/";
+
+%Each method
 methods = ["JPEG2000", "Gaussian", "Poisson", "SGCK_GAMUT_MAPPING"];
 
 figure();
+
+%For each method 
 for j=1:length(methods)
     method = methods(j);
     reprod_path_prefix = "Images/Reproduction/" + method + "/";
+
+    %Empty list of scores 
     Quality_index = [];
-    SSI_score = [];
+    SSIM_score = [];
+    
+    %For each of the images, add the score of each image to the lists
     for i=1:10
         original_path = original_path_prefix + i + ".bmp";
         reprod_path = reprod_path_prefix + i + ".bmp";
         Quality_index = [Quality_index, image_score(original_path,reprod_path)];
-        SSI_score = [SSI_score, ssim(imread(reprod_path),imread(original_path))];
+        SSIM_score = [SSIM_score, ssim(imread(reprod_path),imread(original_path))];
     end
+
+    % Compare our quality score to the SSIM score, and add it to the
+    % subplot
     subplot(length(methods),1,j);
     hold on;
-    plot(SSI_score)
+    plot(SSIM_score)
     plot(Quality_index)
     hold off;
     xlabel("Images");
     ylabel("Score");
     title("Compare Scores to SSIM for " + method + " Images");
     legend("SSIM", "Our Quality Index");
+
+    
 end
